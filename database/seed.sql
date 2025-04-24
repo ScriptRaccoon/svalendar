bc198101e9ad09ac79ab809c01363949d4601a89
Script Raccoon <scriptraccoon@gmail.com>
2025-04-24 17:25:40 +0200

    adjust schemas

--- database/seed.sql
+++ database/seed.sql
@@ -1,12 +1,33 @@
-CREATE TABLE IF NOT EXISTS users (name TEXT NOT NULL UNIQUE, password_hash TEXT NOT NULL);
+CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL UNIQUE,
+    password_hash TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    last_login TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS calendars (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    color TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    user_id INTEGER NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users (id)
+);
+
+CREATE INDEX IF NOT EXISTS idx_user_id ON calendars (user_id);
 
 CREATE TABLE IF NOT EXISTS event (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
+    calendar_id INTEGER NOT NULL,
     title TEXT NOT NULL,
     description TEXT,
     start_time TEXT NOT NULL,
     end_time TEXT NOT NULL,
     location TEXT,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    color TEXT NOT NULL
-)
+    color TEXT NOT NULL,
+    FOREIGN KEY (calendar_id) REFERENCES calendars (id)
+);
+
+CREATE INDEX IF NOT EXISTS idx_calendar_id ON event (calendar_id);
