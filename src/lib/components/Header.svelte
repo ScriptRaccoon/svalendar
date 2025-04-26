f0abdec8b8e8433ebff0447437d8d28102675059
Script Raccoon <scriptraccoon@gmail.com>
2025-04-26 02:39:02 +0200

    remove icons from header

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,14 +1,5 @@
 <script lang="ts">
 	import { page } from '$app/state'
-	import {
-		faCog,
-		faHome,
-		faList,
-		faSignIn,
-		faSignOut,
-		faUserPlus
-	} from '@fortawesome/free-solid-svg-icons'
-	import Fa from 'svelte-fa'
 </script>
 
 <header>
@@ -17,28 +8,22 @@
 		<ul>
 			{#if !page.data.user}
 				<li>
-					<Fa icon={faHome} />
 					<a href="/"> Home </a>
 				</li>
 				<li>
-					<Fa icon={faUserPlus} />
 					<a href="/register"> Register </a>
 				</li>
 				<li>
-					<Fa icon={faSignIn} />
 					<a href="/login"> Login </a>
 				</li>
 			{:else}
 				<li>
-					<Fa icon={faList} />
 					<a href="/app/dashboard">Dashboard</a>
 				</li>
 				<li>
-					<Fa icon={faCog} />
 					<a href="/account">Account</a>
 				</li>
 				<li>
-					<Fa icon={faSignOut} />
 					<a href="/logout" data-sveltekit-preload-data="off">Logout</a>
 				</li>
 			{/if}
@@ -51,7 +36,7 @@
 		display: flex;
 		justify-content: space-between;
 		align-items: center;
-		padding-block: 1rem;
+		padding-block: 1rem 1.5rem;
 	}
 
 	h1 {
