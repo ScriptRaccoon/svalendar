5b05cf122940c18fca85e01e512cbb19d652dcd1
Script Raccoon <scriptraccoon@gmail.com>
2025-04-28 22:05:13 +0200

    show location in event preview

--- src/lib/components/EventPreview.svelte
+++ src/lib/components/EventPreview.svelte
@@ -1,7 +1,18 @@
 <script lang="ts">
 	import { EVENTS_COLORS_DICTIONARY } from '$lib/config'
 	import type { CalendarEvent } from '$lib/server/types'
+	import { faClockFour } from '@fortawesome/free-regular-svg-icons'
+	import {
+		faClock,
+		faLocation,
+		faLocationDot,
+		faLocationPin,
+		faLocationPinLock,
+		faMapLocation,
+		faSearchLocation
+	} from '@fortawesome/free-solid-svg-icons'
 	import { format, differenceInMinutes } from 'date-fns'
+	import Fa from 'svelte-fa'
 
 	type Props = {
 		event: CalendarEvent
@@ -25,10 +36,19 @@
 	href={`/app/calendar/${calendar_id}/event/${event.id}`}
 	style="--color: {EVENTS_COLORS_DICTIONARY[event.color]}; --mins: {length_in_minutes}"
 >
-	<div class="title">
-		{event.title}
+	<div class="header">
+		<div class="title">
+			{event.title}
+		</div>
+		{#if event.location}
+			<div class="location">
+				<Fa icon={faLocationDot} />
+				{event.location}
+			</div>
+		{/if}
 	</div>
 	<div class="time">
+		<Fa icon={faClockFour} />
 		{format(event.start_time, 'HH:mm')} &ndash;
 		{format(event.end_time, 'HH:mm')}
 	</div>
@@ -49,8 +69,18 @@
 		min-height: calc(0.1rem * var(--mins));
 	}
 
+	.header {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+	}
+
 	.title {
 		font-weight: bold;
+	}
+
+	.location {
+		font-size: 0.825rem;
 	}
 
 	.spacer {
