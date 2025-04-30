d7a95be85441265a85e5e4874db541045357995e
Script Raccoon <scriptraccoon@gmail.com>
2025-04-30 21:00:48 +0200

    custom error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,11 @@
+<script>
+	import { page } from '$app/state'
+</script>
+
+<svelte:head>
+	<title>{page.status} Error</title>
+</svelte:head>
+
+<h2>{page.status} Error</h2>
+
+<p>{page.error?.message}</p>
