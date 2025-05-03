3ee428904614d0a846b7a8a783f07ac75e17abb1
Script Raccoon <scriptraccoon@gmail.com>
2025-05-03 13:37:47 +0200

    introduce layout group

---
+++ src/routes/(pages)/+layout.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import Header from '$lib/components/Header.svelte'
+
+	let { children } = $props()
+</script>
+
+<div class="wrapper">
+	<Header />
+	<main>
+		{@render children?.()}
+	</main>
+</div>
+
+<style>
+	.wrapper {
+		padding-inline: 0.75rem;
+		max-width: 42rem;
+		margin-inline: auto;
+		margin-bottom: 2rem;
+	}
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,22 +1,7 @@
 <script lang="ts">
-	import Header from '$lib/components/Header.svelte'
 	import './app.css'
 
 	let { children } = $props()
 </script>
 
-<div class="wrapper">
-	<Header />
-	<main>
-		{@render children?.()}
-	</main>
-</div>
-
-<style>
-	.wrapper {
-		padding-inline: 0.75rem;
-		max-width: 42rem;
-		margin-inline: auto;
-		margin-bottom: 2rem;
-	}
-</style>
+{@render children?.()}
