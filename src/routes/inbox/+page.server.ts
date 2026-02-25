fc8c0f8055ab15e3af0a461cecd04f4f531a38e1
Script Raccoon <scriptraccoon@gmail.com>
2026-02-25 12:07:21 +0100

    archive and unarchive notifications

--- src/routes/inbox/+page.server.ts
+++ src/routes/inbox/+page.server.ts
@@ -1,5 +1,5 @@
-import { error } from '@sveltejs/kit'
-import type { PageServerLoad } from './$types'
+import { error, fail } from '@sveltejs/kit'
+import type { Actions, PageServerLoad } from './$types'
 import sql from 'sql-template-tag'
 import { query } from '$lib/server/db'
 import type { Notification } from '$lib/server/types'
@@ -8,15 +8,60 @@ export const load: PageServerLoad = async (event) => {
 	const user = event.locals.user
 	if (!user) error(401, 'Unauthorized')
 
-	const notifications_query = sql`
+	const show_archived = event.url.searchParams.get('filter') === 'archived'
+
+	const notifications_query_archived = sql`
     SELECT id, sender_id, type, status, message, created_at, event_id
     FROM notifications
-    WHERE recipient_id = ${user.id}
-	ORDER BY created_at DESC
-    `
+    WHERE recipient_id = ${user.id} AND status = 'archived'
+	ORDER BY created_at DESC`
+
+	const notification_query_unarchived = sql`
+    SELECT id, sender_id, type, status, message, created_at, event_id
+    FROM notifications
+    WHERE recipient_id = ${user.id} AND status != 'archived'
+	ORDER BY created_at DESC`
+
+	const notifications_query = show_archived
+		? notifications_query_archived
+		: notification_query_unarchived
 
 	const { err, rows: notifications } = await query<Notification>(notifications_query)
 	if (err) error(500, 'Database error')
 
-	return { notifications }
+	return { show_archived, notifications }
+}
+
+export const actions: Actions = {
+	archive: async (event) => {
+		const user = event.locals.user
+		if (!user) error(401, 'Unauthorized')
+
+		const form = await event.request.formData()
+		const id = form.get('id') as string
+
+		const archive_sql = sql`
+		UPDATE notifications
+		SET status = 'archived'
+		WHERE id = ${id} AND recipient_id = ${user.id}`
+
+		const { err } = await query(archive_sql)
+		if (err) return fail(500, { error: 'Database error' })
+	},
+
+	unarchive: async (event) => {
+		const user = event.locals.user
+		if (!user) error(401, 'Unauthorized')
+
+		const form = await event.request.formData()
+		const id = form.get('id') as string
+
+		const unarchive_sql = sql`
+		UPDATE notifications
+		SET status = 'read'
+		WHERE id = ${id} AND recipient_id = ${user.id}`
+
+		const { err } = await query(unarchive_sql)
+		if (err) return fail(500, { error: 'Database error' })
+	}
 }
--- src/routes/inbox/+page.svelte
+++ src/routes/inbox/+page.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
+	import { enhance } from '$app/forms'
 	import AppMenu from '$lib/components/AppMenu.svelte'
 
-	let { data } = $props()
+	let { data, form } = $props()
 </script>
 
 <svelte:head>
@@ -13,6 +14,41 @@
 	<AppMenu />
 </header>
 
+{#if data.show_archived}
+	<a href="/inbox">Unarchived notifications</a>
+{:else}
+	<a href="/inbox?filter=archived">Archived notifications</a>
+{/if}
+
 <!-- TODO: add proper UI + CRUD -->
 
-<pre>{JSON.stringify(data.notifications, null, 4)}</pre>
+{#if data.notifications.length > 0}
+	{#each data.notifications as notification (notification.id)}
+		<div class="box">
+			<form method="POST" use:enhance>
+				<input type="hidden" name="id" value={notification.id} />
+				<pre>{JSON.stringify(notification, null, 4)}</pre>
+				<br />
+				{#if data.show_archived}
+					<button class="button" formaction="?/unarchive">Unarchive</button>
+				{:else}
+					<button class="button" formaction="?/archive">Archive</button>
+				{/if}
+			</form>
+		</div>
+	{/each}
+{:else}
+	<p>No notifications.</p>
+{/if}
+
+{#if form?.error}
+	<div class="error">{form.error}</div>
+{/if}
+
+<style>
+	/* TEMPORARY */
+	.box {
+		padding-block: 1rem;
+		border-bottom: 1px solid var(--helping-line-color);
+	}
+</style>
