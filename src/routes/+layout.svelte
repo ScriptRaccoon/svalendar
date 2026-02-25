c1ae0ddfe374acdaf05e0af95feb7a3918781f83
Script Raccoon <scriptraccoon@gmail.com>
2026-02-25 01:47:48 +0100

    move head to layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,4 +4,29 @@
 	let { children } = $props()
 </script>
 
+<svelte:head>
+	<link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+	<link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+	<link rel="shortcut icon" href="/favicon.ico" />
+	<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+	<meta name="apple-mobile-web-app-title" content="Svalendar" />
+	<link rel="manifest" href="/site.webmanifest" />
+
+	<meta property="og:title" content="Svalendar" />
+	<meta
+		property="og:description"
+		content="Calendar application for managing events and schedules."
+	/>
+	<meta property="og:type" content="website" />
+	<meta property="og:url" content="https://svalendar.netlify.app" />
+
+	<link
+		rel="preload"
+		href="/fonts/OpenSans-VariableFont_wdth,wght.woff2"
+		as="font"
+		type="font/woff2"
+		crossorigin="anonymous"
+	/>
+</svelte:head>
+
 {@render children?.()}
